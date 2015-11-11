51fb37101e747907b32c3b06cecba89c106c62aa
Tulun <jaykiraly@gmail.com>
2015-11-11 14:53:09 -0800

    Server side done.

--- src/reducer.js
+++ src/reducer.js
@@ -1,17 +1,3 @@
-import {setEntries, next, vote} from './core';
-
-export default function reducer(state, action) {
-  switch (action.type) {
-  case 'SET_ENTRIES':
-    return setEntries(state, action.entries);
-  case 'NEXT':
-    return next(state);
-  case 'VOTE':
-    return vote(state, action.entry)
-  }
-  return state;
-}
-
 import {setEntries, next, vote, INITIAL_STATE} from './core';
 
 export default function reducer(state = INITIAL_STATE, action) {
